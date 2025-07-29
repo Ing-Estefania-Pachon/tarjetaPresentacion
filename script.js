c2c0ec5b9a07a7570297352f67f4b7cca6f0c345
Ing-Estefania-Pachon <ing.estefaniapachon@gmail.com>
2025-07-29 13:12:58 -0500

    Agregue funcion al boton

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 // Mostrar un mensaje simple en la consola
 document.addEventListener("DOMContentLoaded", function () {
   alert("¡Hola, desarrolladora Fullstack en camino! 🎓💜");
+
+  // Seleccionar el botón por su ID y agregar evento
+  const boton = document.getElementById("click");
+  boton.addEventListener("click", function () {
+    alert("Se ha enviado un mensaje");
+  });
+
 });
